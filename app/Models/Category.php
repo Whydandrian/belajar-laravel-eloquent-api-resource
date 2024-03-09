f8f2114e30a028d7dbb130792a8c739369946cc9
Whydandrian <whydandrian@gmail.com>
2024-03-09 19:55:34 +0700

    Model: definition properties table categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+   protected $table = 'categories';
+   protected $primaryKey = 'id';
+   protected $keyType = 'int';
+   public $incrementing = true;
+   public $timestamps = true;
 }
