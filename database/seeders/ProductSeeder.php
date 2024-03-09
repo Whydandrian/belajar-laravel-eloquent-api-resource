9891c97d47d465170a1c46b3e3db69f0ef54466b
Whydandrian <whydandrian@gmail.com>
2024-03-09 22:50:01 +0700

    Add loop for create data product

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+   public function run(): void
+   {
+      Category::all()->each(function (Category $category) {
+         for ($i = 0; $i < 5; $i++) {
+            $category->products()->create([
+               "name" => "Product $i of $category->name",
+               "price" => rand(100, 1000)
+            ]);
+         }
+      });
+   }
 }
