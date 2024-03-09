375ba76c57333fd9ae4d273383f7d2a24ece44ba
Whydandrian <whydandrian@gmail.com>
2024-03-09 19:46:08 +0700

    Make properties category table & migrate

--- database/migrations/2024_03_09_122135_create_categories_table.php
+++ database/migrations/2024_03_09_122135_create_categories_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up(): void
+   {
+      Schema::create('categories', function (Blueprint $table) {
+         $table->id();
+         $table->string('name', 100)->nullable(false);
+         $table->text('description')->nullable();
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('categories');
-    }
+
+   public function down(): void
+   {
+      Schema::dropIfExists('categories');
+   }
 };
