80e911a4023b259a848f5773741669af7b9d050e
Whydandrian <whydandrian@gmail.com>
2024-03-09 23:00:55 +0700

    create product test & add testing datawrap

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+   public function testProductDataWrap()
+   {
+      $this->seed([CategorySeeder::class, ProductSeeder::class]);
+      $products = Product::first();
+
+      $this->get("/api/products/$products->id")
+         ->assertStatus(200)
+         ->assertJson([
+            "value" => [
+               "name" => $products->name,
+               "category" => [
+                  "id" => $products->category->id,
+                  "name" => $products->category->name,
+               ],
+               "price" => $products->price,
+               "created_at" => $products->created_at->toJson(),
+               "updated_at" => $products->updated_at->toJson(),
+            ]
+         ]);
+
+      return $this->any();
+   }
+}
