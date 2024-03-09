eb48aed27d858bf6a5f8f27d7bfe3b3a3aa2948f
Whydandrian <whydandrian@gmail.com>
2024-03-10 01:30:53 +0700

    Create debugresource & add property additional

---
+++ app/Http/Resources/ProductDebugResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Product
+ */
+class ProductDebugResource extends JsonResource
+{
+   public $additional = [
+      "author" => "whydandrian"
+   ];
+   public function toArray(Request $request): array
+   {
+      return [
+         "id" => $this->id,
+         "name" => $this->price,
+         "price" => $this->price,
+      ];
+   }
+}
