96b4b334b4c1125408ac2305fb530c42ab5249f9
Whydandrian <whydandrian@gmail.com>
2024-03-09 20:01:40 +0700

    Migration: definition properties for table product & migrate success

--- database/migrations/2024_03_09_125606_create_products_table.php
+++ database/migrations/2024_03_09_125606_create_products_table.php
@@ -10,7 +10,12 @@ return new class extends Migration
    {
       Schema::create('products', function (Blueprint $table) {
          $table->id();
+         $table->string('name', 100)->nullable(false);
+         $table->bigInteger('price')->nullable(false)->default(0);
+         $table->integer('stock')->nullable(false)->default(0);
+         $table->unsignedBigInteger('category_id')->nullable(false);
          $table->timestamps();
+         $table->foreign('category_id')->references('id')->on('categories');
       });
    }
 
