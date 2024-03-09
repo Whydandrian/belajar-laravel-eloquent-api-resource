c90d1c55b336b71f3305085c761ffb160999c0c6
Whydandrian <whydandrian@gmail.com>
2024-03-09 20:07:30 +0700

    Model: def. properties product & add relation to category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    use HasFactory;
+   protected $table = 'products';
+   protected $primaryKey = 'id';
+   protected $keyType = 'int';
+   public $incrementing = true;
+   public $timestamps = true;
+
+   // Add relation to table categories : 1 to Many
+   public function category(): BelongsTo
+   {
+      return $this->belongsTo(Category::class, 'category_id', 'id');
+   }
 }
