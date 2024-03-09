893ec3e115fc60b6dc1c36b67e9336ce99d43408
Whydandrian <whydandrian@gmail.com>
2024-03-10 01:33:19 +0700

    add route for product debug

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Resources\CategoryCollection;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductDebugResource;
 use App\Http\Resources\ProductResource;
 use App\Models\Category;
 use App\Models\Product;
@@ -53,4 +54,9 @@ Route::get('/products-paging', function (Request $request) {
    $page = $request->get("page", 1);
    $products = Product::paginate(perPage: 2, page: $page);
    return new ProductCollection($products);
+});
+
+Route::get('/products-debug/{id}', function ($id) {
+   $product = Product::find($id);
+   return new ProductDebugResource($product);
 });
